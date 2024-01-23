f5a27395711f7c9e59ac3e3d7ea25013474f25b2
Максим <garkusha.06@bk.ru>
2024-01-23 12:14:33 +0600

    обнова

--- startedTasks/task1.cpp
+++ startedTasks/task1.cpp
@@ -9,16 +9,19 @@ int calculate(string mathExpression){
     istringstream iss(mathExpression);
     char currentChar;
 
-    const int MAX_COUNT_NUMBERS = 100;
-    const int MAX_COUNT_OPERATORS = 99;
+    const int maxCountNumbers = 100;
+    const int maxCountOperators = 99;
 
-    double peremenie[MAX_COUNT_NUMBERS];
-    char operators[MAX_COUNT_OPERATORS];
+    double peremenie[maxCountNumbers];
+    char operators[maxCountOperators];
+    char operators_prav[100];
 
     int count_numbers = 0;
     int count_operators = 0;
 
     bool correct = true;
+    bool pravilo = false;
+    bool prav2 = false;
 
     while (iss >> currentChar) {
         if (isdigit(currentChar) || currentChar == '.') {
@@ -33,6 +36,10 @@ int calculate(string mathExpression){
             // Операторы
             operators[count_operators] = currentChar;
             count_operators++;
+            if (currentChar == '*' || currentChar == '/') {
+                pravilo = true;
+                operators_prav[count_operators] = currentChar;
+            }
         }
         else{
             correct = false;
@@ -43,25 +50,99 @@ int calculate(string mathExpression){
         cout << "Неверный ввод";
         return 0;
     }
-    double result = peremenie[0];
-    for (int i = 0; i < count_operators; i++) {
-        switch (operators[i]) {
-            case '/':
-                result /= peremenie[i + 1];
-                break;
-            case '*':
-                result *= peremenie[i + 1];
-                break;
-            case '+':
-                result += peremenie[i + 1];
-                break;
-            case '-':
-                result -= peremenie[i + 1];
-                break;
-            case '^':
-                result = pow(result,peremenie[i + 1]);
-                break;
+    int j = 0, jj = 0;
+    double result = 0;
+    double predresult[count_numbers];
+    char predoper[maxCountOperators];
+    for (int i = 0; i < 100; i++) {
+        if (operators[i - 1] == '*' && operators[i + 1] == '/' || operators[i - 1] == '/' && operators[i + 1] == '*' ||
+            operators[i - 1] == '*' && operators[i + 1] == '*' || operators[i - 1] == '/' && operators[i + 1] == '/') {
+            prav2 = true;
+            jj++;
         }
+    }
+
+    if(prav2){
+        for (int i = 0; i < count_operators; i++) {
+            switch (operators[i]) {
+                case '/':
+                    predresult[i] = peremenie[i] / peremenie[i + 1];
+                    break;
+                case '*':
+                    predresult[i] = peremenie[i] * peremenie[i + 1];
+                    break;
+                default:
+                    predresult[i] = peremenie[i];
+                    predoper[j]=operators[i];
+                    j++;
+            }
+        }
+        result = predresult[0];
+        if(jj<2){
+            for (int i = 0; i < j; i++) {
+                switch (predoper[i]) {
+                    case '+':
+                        result = result + predresult[i + 2];
+                        break;
+                    case '-':
+                        result = result - predresult[i + 2];
+                        break;
+                }
+            }
+        }else{
+            cout<<"Решать такое я не умею, но попытатся решить могу: "<<endl;
+        }
+    }else if (pravilo) {
+        for (int i = 0; i < count_operators; i++) {
+            switch (operators[i]) {
+                case '/':
+                    predresult[i] = peremenie[i] / peremenie[i + 1];
+                    jj++;
+                    break;
+                case '*':
+                    predresult[i] = peremenie[i] * peremenie[i + 1];
+                    jj++;
+                    break;
+                default:
+                    predresult[i] = peremenie[i];
+                    predoper[j]=operators[i];
+                    j++;
+            }
+        }
+
+        result = predresult[0];
+        for (int i = 0; i < j; i++) {
+            switch (predoper[i]) {
+                case '+':
+                    result = result + predresult[i + 1];
+                    break;
+                case '-':
+                    result = result - predresult[i + 1];
+                    break;
+            }
+        }
+    } else {
+        result = peremenie[0];
+        for (int i = 0; i < count_operators; i++) {
+            switch (operators[i]) {
+                case '/':
+                    result /= peremenie[i + 1];
+                    break;
+                case '*':
+                    result *= peremenie[i + 1];
+                    break;
+                case '+':
+                    result += peremenie[i + 1];
+                    break;
+                case '-':
+                    result -= peremenie[i + 1];
+                    break;
+                case '^':
+                    result = pow(result,peremenie[i + 1]);
+                    break;
+            }
+        }
+
     }
     return result;
 }
